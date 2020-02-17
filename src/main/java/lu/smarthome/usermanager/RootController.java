bf2ef55a874a51892c6b7cffdb9e67ff184415d6
Rodislav MOLDOVAN <rodislav@gmail.com>
2020-02-17 01:04:05 +0100

    using root controllers for things

--- src/main/java/lu/smarthome/usermanager/RootController.java
+++ src/main/java/lu/smarthome/usermanager/RootController.java
@@ -1,20 +1,30 @@
 package lu.smarthome.usermanager;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("${app.api.version.v1}/me")
+@RequestMapping("/")
 @CrossOrigin("${app.api.cors}")
-public class MeController {
+public class RootController {
 
-    @GetMapping
+    @Value("${app.api.version.default}")
+    private String defaultApiSpecUri;
+
+    @GetMapping("/me")
+    public RedirectView redirectApiToSwagger() {
+        return new RedirectView(defaultApiSpecUri + "/me");
+    }
+
+    @GetMapping("${app.api.version.default}/me")
     public Principal getMe(Principal p) {
         return p;
     }
