3fce13d1bf4000bf1b544c00998afadcd718e80c
Rodislav MOLDOVAN <rodislav@gmail.com>
2020-02-21 01:39:45 +0100

    small improvements for api and swagger ui

--- src/main/java/lu/smarthome/usermanager/RootController.java
+++ src/main/java/lu/smarthome/usermanager/RootController.java
@@ -1,26 +1,30 @@
 package lu.smarthome.usermanager;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/")
 @CrossOrigin("${app.api.cors}")
 public class RootController {
 
     @Value("${app.api.version.default}")
     private String defaultApiSpecUri;
 
-    @GetMapping("/me")
+    @GetMapping
+    public RedirectView redirectRootToSwagger() {
+        return new RedirectView("/api");
+    }
+
+    @GetMapping("/api")
     public RedirectView redirectApiToSwagger() {
-        return new RedirectView(defaultApiSpecUri + "/me");
+        return new RedirectView("/webjars/swagger-ui/index.html?url=" + defaultApiSpecUri + ".yml");
+    }
+
+    @GetMapping("me")
+    public Object getMe(Principal p) {
+        return p;
     }
 }
