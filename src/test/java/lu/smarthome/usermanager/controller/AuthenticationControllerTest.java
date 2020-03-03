9491bd785ac9347db8ea9c2813b4c448cef0ca0f
Rowebsoft <rowebsoft@gmail.com>
2020-03-03 22:35:03 +0100

    added 401 expected test for auth controller

---
+++ src/test/java/lu/smarthome/usermanager/controller/AuthenticationControllerTest.java
@@ -0,0 +1,31 @@
+package lu.smarthome.usermanager.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles(profiles = {"test"})
+class AuthenticationControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void authenticate() throws Exception {
+        mvc.perform(
+                get("/authenticate"))
+                .andExpect(status().is(HttpStatus.UNAUTHORIZED.value()));
+    }
+}
