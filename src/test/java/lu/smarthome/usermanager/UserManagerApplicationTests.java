d02502f24d6c870722121d711d8d00670bc47ec2
Rowebsoft <rowebsoft@gmail.com>
2020-03-03 22:24:59 +0100

    fixed the tests inside the user-manager repo

--- src/test/java/lu/smarthome/usermanager/UserManagerApplicationTests.java
+++ src/test/java/lu/smarthome/usermanager/UserManagerApplicationTests.java
@@ -2,12 +2,14 @@ package lu.smarthome.usermanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = {"test"})
 class UserManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
