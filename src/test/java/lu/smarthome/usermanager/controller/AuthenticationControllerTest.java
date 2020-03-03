930f520688b532df20ff9a0f96e8f2364cbcea2b
Rowebsoft <rowebsoft@gmail.com>
2020-03-03 22:38:10 +0100

    added 200 expected test for auth controller

--- src/test/java/lu/smarthome/usermanager/controller/AuthenticationControllerTest.java
+++ src/test/java/lu/smarthome/usermanager/controller/AuthenticationControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,7 +24,15 @@ class AuthenticationControllerTest {
     private MockMvc mvc;
 
     @Test
+    @WithMockUser("customUsername")
     void authenticate() throws Exception {
+        mvc.perform(
+                get("/authenticate"))
+                .andExpect(status().is(HttpStatus.OK.value()));
+    }
+
+    @Test
+    void authenticateAndExpect401() throws Exception {
         mvc.perform(
                 get("/authenticate"))
                 .andExpect(status().is(HttpStatus.UNAUTHORIZED.value()));
