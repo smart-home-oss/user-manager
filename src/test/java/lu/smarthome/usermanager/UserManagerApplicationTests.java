93f7638bc1b78a5a7e6857b9ccc566e56420ac03
Rodislav MOLDOVAN <rodislav@gmail.com>
2020-09-23 02:30:59 +0200

    added bootstrap.properties, bump parent version

--- src/test/java/lu/smarthome/usermanager/UserManagerApplicationTests.java
+++ src/test/java/lu/smarthome/usermanager/UserManagerApplicationTests.java
@@ -1,11 +1,14 @@
 package lu.smarthome.usermanager;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles(profiles = {"test"})
+@TestPropertySource(value = {"classpath:application.properties","classpath:application-test.properties"})
 class UserManagerApplicationTests {
 
     @Test
